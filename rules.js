1899181393f7ff7cd054e51a16bdb74337f7bac8
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-09-28 10:53:10 -0430

    Improved rules style.

---
+++ rules.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {
+        counts: 10,
+        language: 'spanish',
+        text: 'Regla 1: Nunca olvide que la persona que lee el mensaje es otro ser humano con sentimientos que pueden ser lastimados.\n\nRegla 2: Adhiérase a los mismos estándares de comportamiento en línea que usted sigue en la vida real.\n\nRegla 3: Escribir todo en mayúsculas se considera como gritar y además, dificulta la lectura.\n\nRegla 4: Respete el tiempo y el ancho de banda de otras personas.\n\nRegla 5: Muestre el lado bueno de sí mismo mientras se mantenga en línea.\n\nRegla 6: Comparta con la comunidad.\n\nRegla 7: Ayude a mantener los debates en un ambiente sano y educativo.\n\nRegla 8: Respete la privacidad de terceras personas, hacer un grupo contra una persona está mal.\n\nRegla 9: No abuse de su poder (ordenador).\n\nRegla 10: Sea objetivo sobre temas cuyo bien primordial no afecte el general.'
+    }
+];
